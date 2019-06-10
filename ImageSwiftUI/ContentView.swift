46d8f5f10238f1427c165cf6cd06a01ce222498e
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2019-06-10 11:38:21 +0200

    completed

--- ImageSwiftUI/ContentView.swift
+++ ImageSwiftUI/ContentView.swift
@@ -10,14 +10,53 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            ZStack {
+                Image("sansa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .relativeSize(width: 1, height: 1)
+                    .clipped()
+                    .blur(radius: 10, opaque: true)
+                
+                Image("sansa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .relativeSize(width: 0.4, height: 0.4)
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Sansa Stark")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("Lady of Winterfell")
+                    .font(.headline)
+                
+                Text("Sansa Stark is a fictional character created by American author George R. R. Martin. She is a prominent character in Martin's award-winning A Song of Ice and Fire series.")
+                    .lineLimit(nil)
+                    .padding(.top)
+                
+                }
+                .padding()
+            
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            
+            ContentView()
+                .previewDevice("iPad Pro (9.7-inch)")
+        }
     }
 }
 #endif
